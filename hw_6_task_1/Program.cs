4b247c15f977992fd5f18efc05fba41e9644c4db
Kristina <zkvzkvzkv@yandex.ru>
2023-02-01 23:35:48 +0300

    создана программа по условию задачи 1

--- hw_6_task_1/Program.cs
+++ hw_6_task_1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getUserData (string message)
+{
+    Console.WriteLine(message);
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+
+int lenght = getUserData("Введите количество чисел, которое хотите ввести: ");
+int [] numbers = new int [lenght];
+
+for (int i = 0; i < lenght; i++)
+{
+    Console.WriteLine("Введите число: ");
+    numbers [i] = int.Parse(Console.ReadLine()!);
+}
+
+int PositiveNumbers(int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int positiveNumbersCount = PositiveNumbers(numbers);
+Console.WriteLine($"Количество чисел больше 0 равно {positiveNumbersCount}");
